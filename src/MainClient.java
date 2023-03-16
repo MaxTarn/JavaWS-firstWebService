e60c88f6379bc1160740a2fc48ba5a452fe60c01
Maxiii <maxtharn@gmail.com // max.tarn@gritacademy.se>
2023-03-16 10:20:34 +0100

    it should connect to server and function

--- src/MainClient.java
+++ src/MainClient.java
@@ -1,6 +1,23 @@
 import JavaClasses.Client;
+
+import java.util.Scanner;
+
 public class MainClient {
     public static void main(String[] args) {
+        Client client = new Client();
+
+        client.init(6969);
+
+        String messege;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            messege = scan.nextLine();
+            client.sendMessege(messege);
+        }while(messege.equalsIgnoreCase("quit"));
+
+        client.close();
+
 
     }
 }
