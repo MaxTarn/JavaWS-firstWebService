45378543a883f6c5bf614f0a4b9695774c504479
Maxiii <maxtharn@gmail.com // max.tarn@gritacademy.se>
2023-03-16 09:32:03 +0100

    added and made initSockets, initCommunication, getMessege, sendMessege methods

--- src/JavaClasses/Client.java
+++ src/JavaClasses/Client.java
@@ -5,6 +5,57 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
+    Socket socket = null;
+
+    InputStreamReader inputStreamReader = null;
+    BufferedReader bufferedReader = null;
+
+    OutputStreamWriter outputStreamWriter = null;
+    BufferedWriter bufferedWriter = null;
+    String messege = null;
+
+    public void initSockets(int portNumber){
+        try {
+            socket = new Socket("localhost", portNumber);
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void initCommunication(){
+        try {
+            inputStreamReader = new InputStreamReader(socket.getInputStream());
+            bufferedReader = new BufferedReader(inputStreamReader);
+            outputStreamWriter = new OutputStreamWriter(socket.getOutputStream());
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+            Scanner scanner = new Scanner(System.in);
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public String getMessege(){
+        try {
+            messege = bufferedReader.readLine();
+            return messege;
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+            return null;
+        }
+    }
+
+    public void sendMessege(String sendThis){
+        try{
+            bufferedWriter.write(sendThis);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
+
     public static void main(String[] args) {
 
         Socket socket = null;
@@ -41,7 +92,7 @@ public class Client {
                 }
 
             }catch (Exception ex){
-                System.out.println(ex);
+                System.out.println(ex.getMessage());
             }finally {
                 try {
                     if (socket != null) {
