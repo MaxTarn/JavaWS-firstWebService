8233693db6b6660349c703c8ff150b76f10e44ae
Max T <Maxtharn@gmail.com>
2023-03-19 23:59:02 +0100

    creates new Json object, tests file location of person1, prints out the string from person1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-
+import JavaClasses.Json;
 
 public class Main {
     public static void main(String[] args) {
+        Json json = new Json();
+        System.out.println("file exists? : " + json.fileExists(json.getPerson1Path()));
+        System.out.println("String takes from local JSON file: " + json.getStringFromFile(json.getPerson1Path()));
 
     }
 }
