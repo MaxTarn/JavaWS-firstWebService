eb1b9a21cf001b3fc57aad3418e6a9a9b82f42e1
Maxiii <maxtharn@gmail.com // max.tarn@gritacademy.se>
2023-03-23 14:14:22 +0100

    Test initSockets() in Client, not working at the moment

--- src/Tests/ClientTest.java
+++ src/Tests/ClientTest.java
@@ -1,5 +1,6 @@
 package Tests;
 
+import JavaClasses.Client;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,18 @@ public class ClientTest {
     @AfterEach
     void afterEach(){
         System.out.println("after test");
+    }
+
+    @Test
+    void initSocket(){
+        int[] goodPortNumbers = {6969,8080,32000};
+        int[] badPortNumbers = {1,1000, -1,999999999};
+
+        for (int i = 0; i < goodPortNumbers.length; i++) {
+            Client client = new Client();
+            client.initSockets(goodPortNumbers[i]);
+            assertEquals(client.getSocket().getPort(), i);
+        }
     }
 
 
