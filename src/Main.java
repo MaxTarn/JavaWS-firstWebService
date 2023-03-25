e425cfeaae2e491e020c4585c94f5f6ba601a9f1
Max T <Maxtharn@gmail.com>
2023-03-26 00:45:31 +0100

    used for making MakeRequest.java class

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 import JavaClasses.Json;
+import JavaClasses.MakeRequest;
 import org.json.simple.JSONObject;
 
 public class Main {
     public static void main(String[] args) {
         Json json = new Json();
+        MakeRequest makeRequest = new MakeRequest();
         String contentOfFile = json.getStringFromFile(json.getPerson1Path());
 
-        System.out.println(contentOfFile);
-
-
-
-        JSONObject jsonObj = json.convertToJson(contentOfFile);
-
-
+        makeRequest.makeRequest();
     }
 }
