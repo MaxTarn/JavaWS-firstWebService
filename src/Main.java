d6a484867c6d2b2b7acf9c08dc711d4cf02a3bdf
Max T <Maxtharn@gmail.com>
2023-03-27 15:41:28 +0200

    testing getPerson()

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 import JavaClasses.Json;
 import JavaClasses.Request;
+import org.json.simple.JSONObject;
 
 public class Main {
     public static void main(String[] args) {
         Json json = new Json();
         Request request = new Request();
 
-
-        request.makeRequest();
+        JSONObject obj = json.getJsonObjFromFile(json.getJsonPath());
+        json.display(json.getPerson(obj, "1"));
     }
 }
