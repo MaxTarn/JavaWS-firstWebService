606505d7c1b0fe986d777666dea04266bd1f20fb
Max T <Maxtharn@gmail.com>
2023-03-30 15:17:09 +0200

    removed display(), and added alter() that will alter the JSON data in the file

--- src/JavaClasses/Json.java
+++ src/JavaClasses/Json.java
@@ -75,26 +75,8 @@ public class Json {
         }
     }
 
-    public void display(JSONObject person){
-        JSONObject features = (JSONObject) person.get("Features");
 
-        String firstName= person.get("firstName").toString();
-        String lastName = person.get("lastName").toString();
-        String age = person.get("age").toString();
-        String species = person.get("species").toString();
-        String gender = person.get("gender").toString();
-        String hairColor = features.get("hairColor").toString();
-        String eyeColor = features.get("eyeColor").toString();
-        String nose = features.get("nose").toString();
-
-
-        System.out.println("Name: " + firstName + " " + lastName);
-        System.out.println("Age: " + age);
-        System.out.println("Species: " + species);
-        System.out.println("Gender: " + gender);
-        System.out.println("Hair color: " + hairColor);
-        System.out.println("Eye color: " + eyeColor);
-        System.out.println("Nose: " + nose);
+    public void alter(String personId, JSONObject newValues){
 
     }
 
