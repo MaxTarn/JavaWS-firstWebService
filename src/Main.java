6cd0a54520143f404e4ee7426ad7e56682a38430
Max T <Maxtharn@gmail.com>
2023-03-30 20:34:04 +0200

    testing diffrent method

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,12 @@
 import JavaClasses.Json;
-import JavaClasses.Request;
-import org.json.simple.JSONObject;
 
+//used for testing new methods and new code snippets
 public class Main {
 
-    public static void display(JSONObject person){
-        JSONObject features = (JSONObject) person.get("Features");
-
-        String firstName= person.get("firstName").toString();
-        String lastName = person.get("lastName").toString();
-        String age = person.get("age").toString();
-        String species = person.get("species").toString();
-        String gender = person.get("gender").toString();
-        String hairColor = features.get("hairColor").toString();
-        String eyeColor = features.get("eyeColor").toString();
-        String nose = features.get("nose").toString();
-
-
-        System.out.println("Name: " + firstName + " " + lastName);
-        System.out.println("Age: " + age);
-        System.out.println("Species: " + species);
-        System.out.println("Gender: " + gender);
-        System.out.println("Hair color: " + hairColor);
-        System.out.println("Eye color: " + eyeColor);
-        System.out.println("Nose: " + nose);
-
-    }
     public static void main(String[] args) {
         Json json = new Json();
-        Request request = new Request();
 
-        JSONObject obj = json.getJsonObjFromFile(json.getJsonPath());
-        display(json.getPerson(obj, "1"));
+
+
     }
 }
