cf0610826dc1a0d03c8c0df91747432a52abf99d
Max T <Maxtharn@gmail.com>
2023-03-31 13:14:17 +0200

    background files

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import JavaClasses.Json;
+import JavaClasses.Request;
+import org.json.simple.JSONObject;
 
 //used for testing new methods and new code snippets
 public class Main {
 
     public static void main(String[] args) {
         Json json = new Json();
+        Request requestMaker = new Request();
+
+        JSONObject allPersons = json.getJsonObjFromFile(json.getJsonPath());
+        requestMaker.makeAlterRequest(json.getPerson(allPersons, "2"), "2");
+
 
 
 
