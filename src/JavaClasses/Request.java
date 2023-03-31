0011c1d0fb731d5c64ae722a4c570d8d6cf38a34
Max T <Maxtharn@gmail.com>
2023-03-31 13:13:53 +0200

    added makeAlterRequest() that returns a POST request

--- src/JavaClasses/Request.java
+++ src/JavaClasses/Request.java
@@ -36,7 +36,7 @@ public class Request {
             if(temp <= higher && temp >= lower) {
                 goodInput = true;
             }else{
-                System.out.println("Not valid input. Try again.");
+                System.out.print("Not valid input. Try again.");
             }
 
         }while(!goodInput);
@@ -125,21 +125,81 @@ public class Request {
         request.put("URLParameters", "person/" + (chosenEntry));
 
         return request;
-
-
-
-
-
-
     }
 
-    public JSONObject makeAlterRequest(){
-        boolean wantsToAlter = false;
+    public JSONObject makeAlterRequest(JSONObject personToAlter, String personID){
+        JSONObject personToAlterFeatures = (JSONObject) personToAlter.get("Features");
 
-        while(!wantsToAlter){
-            System.out.println("What line do you wish to edit");
+        System.out.print("What line do you wish to edit?   :");
+        int chosenLine = getInputBetween(1, 7);
+        System.out.println();
+
+        String newValue = "";
+        String newFirstName = "";
+        String newLastName = "";
+
+        //on line 1 both first name and last name is printed out
+        if (chosenLine == 1){
+            System.out.print("Change first name to:   ");
+            newFirstName = getInput();
+            System.out.println();
+
+            System.out.print("Change last name to:   ");
+            newLastName = getInput();
+            System.out.println();
+        }else {
+            System.out.print("It will be changed to:   ");
+            newValue = getInput();
+            System.out.println();
         }
-        return null;
+
+        String[] keyNames = {"firstName", "lastName", "age", "species", "gender", "hairColor", "eyeColor", "nose"};
+        switch (chosenLine){
+
+            case 1:
+                personToAlter.replace(keyNames[0], newFirstName);
+                personToAlter.replace(keyNames[1], newFirstName);
+                break;
+            case 2:
+                personToAlter.replace(keyNames[2], newValue);
+                break;
+            case 3:
+                personToAlter.replace(keyNames[3], newValue);
+                break;
+            case 4:
+                personToAlter.replace(keyNames[4], newValue);
+                break;
+            case 5:
+                personToAlterFeatures.replace(keyNames[5], newValue);
+                break;
+            case 6:
+                personToAlterFeatures.replace(keyNames[6], newValue);
+                break;
+            case 7:
+                personToAlterFeatures.replace(keyNames[7], newValue);
+                break;
+
+            default:
+                System.out.println("this should not be printed to console, no matter what.");
+                break;
+        }
+        personToAlter.replace("Features", personToAlterFeatures);
+
+
+        JSONObject request = new JSONObject();
+        request.put("HTTPMethod", "POST");
+        request.put("ContentType", contentType);
+        request.put("URLParameters", "person/" + (personID));
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put(personID, personToAlter);
+
+
+        request.put("body", requestBody);
+
+        System.out.println(request.toString());
+        return  request;
+
     }
 
 }
